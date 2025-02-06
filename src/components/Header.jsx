89a7424ffd9dec7b0d4945c62096091a196bb52c
Ofarouq310 <ofarouq310@gmail.com>
2025-02-06 18:16:39 +0200

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <header>
+            <i className="fa-solid fa-bowl-rice"></i>
+            <h1>Recipe Generator</h1>
+        </header>
+    )
+}
