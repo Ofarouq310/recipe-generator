d3e34efb2c6887c773ab617085542749fcac3d06
Ofarouq310 <ofarouq310@gmail.com>
2025-05-02 22:39:09 +0300

    add ingredients functionality

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react"
+import IngredientsList from "./IngredientsList"
+
 export default function Main() {
+
+    const [ingredients, setIngredients] = useState([])
+
+const addIngredient = (formData) => {
+        const newIngredient = formData.get("ingredient").trim()
+        if (newIngredient.length > 0) {
+            setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+        }
+    }
+
     return (
         <main>
-            <form>
+            <form action={addIngredient} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
+                    required
                 />
                 <button>+ Add ingredient</button>
             </form>
+            <IngredientsList ingredients={ingredients} />
         </main>
     )
 }
