e2843a270fd78b38d8e03826d2b6edcd9701b56e
Ofarouq310 <ofarouq310@gmail.com>
2025-05-04 20:04:22 +0300

    add ai response

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react"
 import IngredientsList from "./IngredientsList"
 import SuggestedRecipe from "./SuggestedRecipe"
+import {getRecipeFromMistral} from "../ai.js"
+
 
 export default function Main() {
 
     const [ingredients, setIngredients] = useState([])
 
-const addIngredient = (formData) => {
+    const addIngredient = (formData) => {
         const newIngredient = formData.get("ingredient").trim()
         if (newIngredient.length > 0) {
             setIngredients(prevIngredients => [...prevIngredients, newIngredient])
         }
+    }
+
+    const [recipe, setRecipe] = useState("")
+
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromMistral(ingredients)
+        setRecipe(recipeMarkdown)
     }
 
     return (
@@ -25,8 +34,8 @@ const addIngredient = (formData) => {
                 />
                 <button>+ Add ingredient</button>
             </form>
-            <IngredientsList ingredients={ingredients} />
-            <SuggestedRecipe />
+            <IngredientsList ingredients={ingredients} getRecipe={getRecipe}  />
+            {recipe && <SuggestedRecipe recipe={recipe} />}
         </main>
     )
 }
--- src/components/SuggestedRecipe.jsx
+++ src/components/SuggestedRecipe.jsx
@@ -1,10 +1,11 @@
-export default function SuggestedRecipe(){
+import ReactMarkdown from 'react-markdown';
+export default function SuggestedRecipe(props){
 
     return (
         <>
             <section className='suggested-recipe-container'>
-                <h2>Suggested Recipe</h2>
-                 
+                <h2>Suggested Recipe:</h2>
+                <ReactMarkdown>{props.recipe}</ReactMarkdown>
             </section>
         </>
     )
